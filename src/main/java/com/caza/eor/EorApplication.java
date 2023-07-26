819492f2ad4a09602f3858f9b6408c2ef26ff239
laLatroni <ivofspadesunique@gmail.com>
2023-07-26 13:43:22 +0800

    first commit

---
+++ src/main/java/com/caza/eor/EorApplication.java
@@ -0,0 +1,13 @@
+package com.caza.eor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/caza/eor/EorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.caza.eor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
